566f23ef26417eaed55af602614df94612abe10e
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-06-06 23:15:16 +0300

    Hello, World!

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // деректива
+
+using namespace std; // використовуємо імена (бібліотеки) std
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello, World!\n";
+
+    cin.get();
+    return 0;
+}
